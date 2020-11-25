e14698b7f8fc3770e3248b947bf5370f82a2f9a7
Tom Van Vleck <thvv@multicians.org>
2020-11-25 09:22:26 -0500

    expandfile3

---
+++ maketar.sh
@@ -0,0 +1,32 @@
+COPYFILE_DISABLE=1 tar cf htmxkit.tar README.txt
+COPYFILE_DISABLE=1 tar rf htmxkit.tar checknonempty
+COPYFILE_DISABLE=1 tar rf htmxkit.tar cr2nl
+COPYFILE_DISABLE=1 tar rf htmxkit.tar csv2sql3.htmt
+COPYFILE_DISABLE=1 tar rf htmxkit.tar expandfile3
+COPYFILE_DISABLE=1 tar rf htmxkit.tar expandfile.man
+COPYFILE_DISABLE=1 tar rf htmxkit.tar expandfile3.pm
+COPYFILE_DISABLE=1 tar rf htmxkit.tar readbindsql3.pm
+COPYFILE_DISABLE=1 tar rf htmxkit.tar readbindxml3.pm
+COPYFILE_DISABLE=1 tar rf htmxkit.tar expandfile_config
+COPYFILE_DISABLE=1 tar rf htmxkit.tar filedaysold
+COPYFILE_DISABLE=1 tar rf htmxkit.tar filemodiso
+COPYFILE_DISABLE=1 tar rf htmxkit.tar filemodshort
+COPYFILE_DISABLE=1 tar rf htmxkit.tar filemodyear
+COPYFILE_DISABLE=1 tar rf htmxkit.tar filesizek
+COPYFILE_DISABLE=1 tar rf htmxkit.tar firstletter
+COPYFILE_DISABLE=1 tar rf htmxkit.tar firstofnextmonth
+COPYFILE_DISABLE=1 tar rf htmxkit.tar fmtnum
+COPYFILE_DISABLE=1 tar rf htmxkit.tar fmtsql
+COPYFILE_DISABLE=1 tar rf htmxkit.tar gifsize
+COPYFILE_DISABLE=1 tar rf htmxkit.tar gifsize2
+COPYFILE_DISABLE=1 tar rf htmxkit.tar gth2x
+COPYFILE_DISABLE=1 tar rf htmxkit.tar gthumb
+COPYFILE_DISABLE=1 tar rf htmxkit.tar htmxlib.htmi
+COPYFILE_DISABLE=1 tar rf htmxkit.tar lowercase
+COPYFILE_DISABLE=1 tar rf htmxkit.tar nargs
+COPYFILE_DISABLE=1 tar rf htmxkit.tar padstring
+COPYFILE_DISABLE=1 tar rf htmxkit.tar gitpush.sh
+COPYFILE_DISABLE=1 tar rf htmxkit.tar uppercase
+COPYFILE_DISABLE=1 tar rf htmxkit.tar xml2sql
+rm -f htmxkit.tar.gz
+gzip htmxkit.tar
